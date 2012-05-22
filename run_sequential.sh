42b5586b8dbc0b120b8653237231788ec80fbcfa
Nick Lacock <nlacock@udel.edu>
2012-05-21 20:01:59 -0400

    Wrote scripts for timing runs for parallel code

--- run_sequential.sh
+++ run_sequential.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-MAX_ITERS=1000
+MAX_ITERS=50000
 
 make
 
 for i in 10 20 30 40 50 60 70 80 90 100; do
 	echo "BOARDSIZE: $i"
 	#srun -N1 --gres=gpu:1 ./ir_sequential $i $MAX_ITERS 
-	./ir_sequential $i $MAX_ITERS | grep -v Q
+	srun -N1 --gres=gpu:1 ./ir_sequential $i $MAX_ITERS | grep -v Q
 	echo -e "\n\n"
 done
